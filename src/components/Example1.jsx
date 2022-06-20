c354009ad84e13dc56810f265272431ba39c088b
Annie Huang <annie.hn@gmail.com>
2022-06-20 21:58:51 +1000

    Add Example1.Example1.jsx

--- src/components/Example1.jsx
+++ src/components/Example1.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import { useSelect } from 'downshift';
+import { items, menuStyles } from './shared';
 
 const Example1 = () => {
+  const {
+    isOpen,
+    selectedItem,
+    getToggleButtonProps,
+    getLabelProps,
+    getMenuProps,
+    highlightedIndex,
+    getItemProps,
+  } = useSelect({ items });
+
   return (
     <div>
-      
+      <label {...getLabelProps()}>Choose an element:</label>
+      <button type='button' {...getToggleButtonProps()}>
+        {selectedItem || 'Elements'}
+      </button>
+      <ul {...getMenuProps()} style={menuStyles}>
+        {isOpen &&
+          items.map((item, index) => (
+            <li
+              style={
+                highlightedIndex === index ? { backgroundColor: '#bde4ff' } : {}
+              }
+              key={`${item}${index}`}
+              {...getItemProps({ item, index })}
+            >
+              {item}
+            </li>
+          ))}
+      </ul>
+      {/* if you Tab from menu, focus goes on button, and it shouldn't. only happens here. */}
+      <div tabIndex='0' />
     </div>
   );
 };
