cbc781cd93b04ab0d1293a274267059920fe7230
Annie Huang <annie.hn@gmail.com>
2022-06-20 22:17:32 +1000

    Try Example2, unsuccessful.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Example1 from './components/Example1';
+import Example2 from './components/Example2';
 
 function App() {
   return (
     <div>
-      <Example1 />
+      {/*<Example1 />*/}
+      <Example2 />
     </div>
   );
 }
