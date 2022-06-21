6963e3ac714a46c3f765640fb042baf1b1f981f5
Annie Huang <annie.hn@gmail.com>
2022-06-21 22:09:45 +1000

    useCombobox Basic Usage <Example5>

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Example1 from './components/useSelectHook/Example1';
 import Example2 from './components/DownshiftComponent/Example2';
 import Example3 from './components/useSelectHook/Example3';
 import Example4 from './components/useSelectHook/Example4';
+import Example5 from './components/useCombobox/Example5';
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       {/*<Example1 />*/}
       {/*<Example2 />*/}
       {/*<Example3 />*/}
-      <Example4 />
+      {/*<Example4 />*/}
+      <Example5 />
     </div>
   );
 }
